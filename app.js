04cb96af1d18da0c5f671d7013ad8fa305dd1289
Marcos Rollon <marcoselric@yahoo.es>
2019-02-17 20:48:49 +0100

    fix 2

--- app.js
+++ app.js
@@ -31,6 +31,11 @@ app.use(bodyParser.json());
 //Setup port variable
 const PORT = process.env.PORT || 8000;
 
+app.get("/", (req, res) => {
+  res.send("Hellou");
+  res.end();
+});
+
 //ROUTES FOR API
 
 let router = express.Router();
